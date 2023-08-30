80ac01ad12912eebfaea7dd5f2288b70712749ed
leandroxbm <leandroximenes10@hotmail.com>
2023-08-30 17:17:56 -0300

    projeto funcionando perfeitamente

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const Button = () => {
+  return (
+    <header>
+        <h1>GitFinder</h1>
+    </header>
+  )
+}
+
+export { Button };
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const Input = () => {
+  return (
+    <header>
+        <h1>GitFinder</h1>
+    </header>
+  )
+}
+
+export { Input };
